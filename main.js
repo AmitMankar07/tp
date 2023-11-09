0870e2f93fa5b73209f21c0f316506c14a63a218
AmitMankar07 <amitmankar071@gmail.com>
2023-11-09 19:53:22 +0530

    add delete and edit btn

---
+++ main.js
@@ -0,0 +1,46 @@
+var form=document.getElementById('addForm');
+var itemList=document.getElementById('items');
+
+
+form.addEventListener('submit',addItem);
+itemList.addEventListener('click',removeItem);
+
+//add item
+function addItem(e){
+    e.preventDefault();
+
+    var newItem=document.getElementById('item').value;
+
+    //new li item
+    var li=document.createElement('li');
+    li.className='list-group-item';
+
+    li.appendChild(document.createTextNode(newItem));
+
+   
+    //delete buttn
+    var deletebtn=document.createElement('button');
+    deletebtn.className='btn btn-danger btn-sm float-right delete';
+    
+    deletebtn.appendChild(document.createTextNode('X'));
+    li.appendChild(deletebtn);
+
+     //edit button
+     var editbutn=document.createElement('button');
+     editbutn.className="btn btn-danger btn-sm float-right Edit";
+     editbutn.appendChild(document.createTextNode('Edit'));
+     li.appendChild(editbutn);
+
+    itemList.appendChild(li);
+}
+
+//rmemove item
+
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are you sure?')){
+            var li=e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
