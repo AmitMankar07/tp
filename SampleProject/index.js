7735795f7585546e58775712ce8bc6cec0039ef3
AmitMankar07 <amitmankar071@gmail.com>
2024-01-11 17:43:00 +0530

    code done

--- SampleProject/index.js
+++ SampleProject/index.js
@@ -1,26 +1,28 @@
 let totalcost=0;
+const ProductPrice=document.getElementById("price").value;
+const ProductName=document.getElementById("name").value;
  function storeToLocal(e)
 {
     e.preventDefault();
     const ProductPrice=document.getElementById("price").value;
     const ProductName=document.getElementById("name").value;
-   
+
     const obj1={
             ProductPrice,
             ProductName
     }
 
-    axios.post('https://crudcrud.com/api/9d54e2405031484a9c37dae1526e4e51/ProductData',obj1)
+    axios.post('https://crudcrud.com/api/f7cf83b460d048bbaf8873cbc049e757/ProductData',obj1)
      .then((res)=>{
         showOnScreen(res.data);
      })
      .catch((err) => console.log(err))
-     
-  
+
+
 }
 
 window.addEventListener("DOMContentLoaded",()=> {
-    axios.get('https://crudcrud.com/api/9d54e2405031484a9c37dae1526e4e51/ProductData')
+    axios.get('https://crudcrud.com/api/f7cf83b460d048bbaf8873cbc049e757/ProductData')
      .then((res) =>{
         for(var i=0;i<res.data.length;i++)
         {
@@ -34,7 +36,7 @@ function showOnScreen(products)
 {
     document.getElementById("price").value='';
     document.getElementById("name").value='';
-    
+
     const productId=products._id;
     const parentNode = document.getElementById('list');
     const childHTML = `<li id=${products._id}> ${products.ProductPrice} - ${products.ProductName} 
@@ -55,111 +57,29 @@ function editUserDetails(ProductPrice,ProductName,productId)
    deleteUser(productId)
 }
 
-function deleteUser(productId)
-{
-    axios.delete(`https://crudcrud.com/api/9d54e2405031484a9c37dae1526e4e51/ProductData/${productId}`).then((response) => {
-            console.log(response);
-            const ProductPrice=document.getElementById(productId).innerText.split('-')[0];
 
-            removeFromScreen(productId,response.data.ProductPrice);
-            })
-        .catch((err) => {
-            console.log(err)
+function deleteUser(productId) {
+    
+    const productToDelete = document.getElementById(productId).innerText.split('-');
+    const ProductPrice = productToDelete[0].trim();
+
+    axios.delete(`https://crudcrud.com/api/f7cf83b460d048bbaf8873cbc049e757/ProductData/${productId}`)
+        .then((response) => {
+            removeFromScreen(productId, ProductPrice);
         })
-
+        .catch((err) => {
+            console.log(err);
+        });
 }
-
-function removeFromScreen(productId)
-{
+function removeFromScreen(productId, ProductPrice) {
     const parentNode = document.getElementById('list');
-                const childNodeToBeDeleted = document.getElementById(productId);
-                if(childNodeToBeDeleted) {
-                    parentNode.removeChild(childNodeToBeDeleted)
-                }
-                totalcost-=Number(ProductPrice);
-                document.getElementById("TotalCost").innerText="Total Cost: "+totalcost;
+    const childNodeToBeDeleted = document.getElementById(productId);
+    
+    if (childNodeToBeDeleted) {
+        parentNode.removeChild(childNodeToBeDeleted);
+    }
+
+    totalcost -= Number(ProductPrice);
+    document.getElementById("TotalCost").innerText = "Total Cost: " + totalcost;
+    console.log(totalcost);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-// let totalprice=0
-// function crud(event){
-//     event.preventDefault();
-//     let price=event.target.num.value;
-//     let name=event.target.name.value;
-
-//     let obj=
-//     {
-//         price:price,
-//         name:name
-//     }
-
-//     axios.post("https://crudcrud.com/api/942de34096ba4b5791e619f130133308/ecom",obj)
-//     .then((res)=>{
-//         showuser(res.data)
-//         console.log(totalprice);
-//         totalprice +=Number(price);
-//         displayTotalPrice(totalprice);
-//     })
-//     .catch((err)=>{
-//         console.log(err)
-//     })
-// }
-// axios.get("https://crudcrud.com/api/942de34096ba4b5791e619f130133308/ecom")
-// .then((res)=>{
-//     totalprice = res.data.reduce((acc, item) => acc + item.price, 0);
-//         displayTotalPrice(totalprice);
-//     for(let i=0;i<res.data.length;i++)
-//     {
-//         console.log(res.data[i]);
-//         showuser(res.data[i]) 
-//     }
-// })
-// .catch((err)=>{
-//     console.log(err)
-// })
-
-// function showuser(obj){
-//     let parent=document.getElementById('listItem');
-//     let child=document.createElement('li');
-//     child.textContent= obj.price+" - "+ obj.name;
-
-//     let deletebut=document.createElement('input');
-//     deletebut.type="button";
-//     deletebut.value="Delete Product"
-
-//     child.appendChild(deletebut);
-//     parent.appendChild(child)
-
-
-//     deletebut.addEventListener('click',deleteuser)
-
-//     function deleteuser(){
-//         axios.delete(`https://crudcrud.com/api/942de34096ba4b5791e619f130133308/ecom/${obj._id}`)
-//         .then(()=>{
-//             parent.removeChild(child);
-//             totalprice -= obj.price;
-//             console.log(totalprice);
-//             displayTotalPrice(totalprice);
-//         })
-//         .catch((err)=>{
-//             console.log(err)
-//         })
-//     }
-
-// }
-
-// function displayTotalPrice(total) {
-//     let totalPriceElement = document.getElementById('TotalCost');
-//     totalPriceElement.textContent = "Total Value worth of Products:" + total; 
-// }
-
