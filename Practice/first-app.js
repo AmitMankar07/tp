b50d152c7eb4a06ebc5908c33e212db7ed742ea7
AmitMankar07 <amitmankar071@gmail.com>
2024-01-14 18:00:10 +0530

    product code

--- Practice/first-app.js
+++ Practice/first-app.js
@@ -1,2 +1,13 @@
 
-console.log("hello nodejs to world")
+const product=(n1,n2)=>{
+    return n1*n2;
+}
+
+console.log(product(10,20));
+
+const Student={
+    name:'Amit',
+    age:22,
+    College:'KITS college'
+}
+console.log(Student);
