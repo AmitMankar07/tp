02dad483bb6f46b9306b54b30874c3e05291807f
AmitMankar07 <amitmankar071@gmail.com>
2024-01-17 19:28:07 +0530

    return res/req nodejs

--- nodejs-pract/play.js
+++ nodejs-pract/play.js
@@ -20,7 +20,7 @@ const server=http.createServer((req,res)=>{
     res.write('</html>')
     return res.end();
    }
-   if(url==='/node'){
+   else if(url==='/node'){
     res.write('<html>');
     res.write('<head><title> My first PAge</title></head>');
     res.write('<body><h1>Welcome to my NodeJs Project</h1></body>');
