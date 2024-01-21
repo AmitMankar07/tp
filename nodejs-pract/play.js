b27809a52704adbbcd7b4422aa14b3d55b6afd86
AmitMankar07 <amitmankar071@gmail.com>
2024-01-21 19:26:05 +0530

    middleware changes

--- nodejs-pract/play.js
+++ nodejs-pract/play.js
@@ -1,18 +1,21 @@
 
 const http=require('http');
-
+const bodyParser=require('body-parser');
 const express=require('express');
 
 const app=express();
 
-app.use((req,res,next)=>{
-    console.log("In the middleware");
-    next();//
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.post('/add-product',(req,res,next)=>{
+    // console.log("In the middleware");
+    res.send('<form action="/product" method="POST">Name:<input type="text" name="title"> Size: <input type="number" name="size"><button type="submit">Add Product</button></form>')  
 });
-app.use((req,res,next)=>{
-    console.log("In the another middleware");
-    res.send('<h1>Hello from Express</h1>');
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
 });
+
 app.listen(4000);
 
 // const server=http.createServer(app);
