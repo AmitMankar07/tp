2e1500e2ef9b4ba9255ccf9b8e7526984668d7c3
AmitMankar07 <amitmankar071@gmail.com>
2024-01-21 17:37:14 +0530

    express js start

--- nodejs-pract/play.js
+++ nodejs-pract/play.js
@@ -1,31 +1,21 @@
 
 const http=require('http');
 
-const routes=require('./routes');
+const express=require('express');
 
-const server=http.createServer(routes);
+const app=express();
 
-server.listen(4000);
+app.use((req,res,next)=>{
+    console.log("In the middleware");
+    next();//
+});
+app.use((req,res,next)=>{
+    console.log("In the another middleware");
+    res.send('<h1>Hello from Express</h1>');
+});
+app.listen(4000);
 
+// const server=http.createServer(app);
 
-// else if(url==='/home'){
-//     res.write('<html>');
-//     res.write('<head><title> My first PAge</title></head>');
-//     res.write('<body><h1>Welcome Home</h1></body>');
-//     res.write('</html>')
-//     return res.end();
-//    }
-//     else if(url==='/about'){
-//     res.write('<html>');
-//     res.write('<head><title> My first PAge</title></head>');
-//     res.write('<body><h1>Welcome to About Us</h1></body>');
-//     res.write('</html>')
-//     return res.end();
-//    }
-//    else if(url==='/node'){
-//     res.write('<html>');
-//     res.write('<head><title> My first PAge</title></head>');
-//     res.write('<body><h1>Welcome to my NodeJs Project</h1></body>');
-//     res.write('</html>')
-//     return res.end();
-//    }
+// server.listen(4000);
+
