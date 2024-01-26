c547b65e9a099af65501508e6163379b33965a99
AmitMankar07 <amitmankar071@gmail.com>
2024-01-26 18:43:52 +0530

    Add Contactus funcnality

--- nodejs-pract/play.js
+++ nodejs-pract/play.js
@@ -1,20 +1,24 @@
-
-const http=require('http');
-const bodyParser=require('body-parser');
+const path=require('path');
+// const http=require('http');
 const express=require('express');
+const bodyParser=require('body-parser');
+
+
 
 const app=express();
 
 const adminRoutes=require('./routes/admin');
 const shopRoutes=require('./routes/shop');
+const contactRoutes=require('./routes/contactus');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/admin',adminRoutes);
-app.use('/shop',shopRoutes);
+app.use(shopRoutes);
+app.use('/contactus',contactRoutes);
 
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page Not Found!</h1>');
+    res.status(404).sendFile(path.join(__dirname,'views','404Error.html'));
 });
 app.listen(4000);
 
--- nodejs-pract/routes/admin.js
+++ nodejs-pract/routes/admin.js
@@ -1,12 +1,17 @@
+const path=require('path');
 const express=require('express');
 
 const router=express.Router();
+const rootDir=require('../util/path');
 
+//get
 router.get('/add-product',(req,res,next)=>{
     // console.log("In the middleware");
-    res.send('<form action="/admin/product" method="POST">Name:<input type="text" name="title"> Size: <input type="number" name="size"><button type="submit">Add Product</button></form>')  
-});
-router.post('/product',(req,res,next)=>{
+    res.sendFile(path.join(__dirname,'../','views','add-product.html'));
+    });
+
+    //POST
+router.post('/add-product',(req,res,next)=>{
     console.log(req.body);
     res.redirect('/');
 }); 
