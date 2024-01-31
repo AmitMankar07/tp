291451511327953e46f5e7c0f68ed98b7ef72c44
AmitMankar07 <amitmankar071@gmail.com>
2024-01-31 14:47:44 +0530

    exuting code with mysql

---
+++ ExpenseApp/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const bodyParser  = require('body-parser');
+const cors = require('cors');
+
+const sequelize  = require('./util/database');
+const expenseRoutes = require('./routes/expense');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use(expenseRoutes)
+
+sequelize.sync()
+.then(result=>{
+    console.log(  "Executing local " );
+    app.listen(3000);
+})
+.catch(err=>
+    {console.log(err);
+})
