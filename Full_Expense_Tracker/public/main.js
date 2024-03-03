d76fb19df98c9472beba757bc2b16dd40c3d453a
AmitMankar07 <amitmankar071@gmail.com>
2024-03-03 19:23:52 +0530

    live update

--- Full_Expense_Tracker/public/main.js
+++ Full_Expense_Tracker/public/main.js
@@ -255,8 +255,41 @@ function parseJwt (token) {
             console.error(error);
         }
     }
+    const showpagination = ({
+        currentPage,
+        hasNextPage,
+        hasPreviousPage,
+        nextPage,
+        previousPage,
+        lastPage,
+    }) => {
+        pagination.innerHTML = "";
+        if (hasPreviousPage) {
+            const btn2 = document.createElement("button");
+            btn2.innerHTML = previousPage;
+            btn2.addEventListener("click", () => showExpenses(previousPage));
+            pagination.appendChild(btn2);
+        }
+        const btn1 = document.createElement("button");
+        btn1.innerHTML = `<h3>${currentPage}</h3>`;
+        btn1.addEventListener("click", () => showExpenses(currentPage));
+        pagination.appendChild(btn1);
     
+        if (hasNextPage) {
+            const btn3 = document.createElement("button");
+            btn3.innerHTML = nextPage;
+            btn3.addEventListener("click", () => showExpenses(nextPage));
+            pagination.appendChild(btn3);
+        }
+        if (lastPage > 2 && lastPage > nextPage) {
+            const btn4 = document.createElement("button");
+            btn4.innerHTML = lastPage;
+            btn4.addEventListener("click", () => showExpenses(lastPage));
+            pagination.appendChild(btn4);
+        }
+    }
     
+
     // Function to clear input fields
     function clearFields() {
         document.getElementById('amount').value = '';
