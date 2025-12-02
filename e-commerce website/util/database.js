4b7dcfd94c6385fff56a997e91a335405ab67d02
AmitMankar07 <amitmankar071@gmail.com>
2025-12-02 19:36:35 +0530

    updated code

---
+++ e-commerce website/util/database.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('e-commerce', 'root', 'Amit1234', {
+  dialect: 'mysql',
+  host: 'localhost'
+});
+
+module.exports = sequelize;
